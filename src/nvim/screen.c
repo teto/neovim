9946c63215da97c042cbfafe40df15e84b7883c6
Matthieu Coudron <mcoudron@hotmail.com>
2021-01-08 10:50:03 +0100

    added api
    introduced win_line2

--- src/nvim/screen.c
+++ src/nvim/screen.c
@@ -1998,7 +1998,8 @@ static int win_line(
     Providers *providers
 )
 {
-  win_line2()
+  return win_line2(wp, lnum, startrow, endrow, nochange, number_only, foldinfo,
+            providers);
 }
 
 static int win_line2(
